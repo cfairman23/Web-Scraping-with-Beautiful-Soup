419f1c9ee12e9e0bf31cccd0f1a6313f42f2db1c
Connor Fairman <connorfairman23@gmail.com>
2017-12-06 23:01:36 -0500

    Updated program to run at 4PM everyday

--- src/nyt_headlines.py
+++ src/nyt_headlines.py
@@ -8,13 +8,45 @@ Created on Nov 24, 2017
 
 from bs4 import BeautifulSoup
 import urllib2
+from datetime import datetime
+from threading import Timer
 
-nytFile = urllib2.urlopen("https://www.nytimes.com/")
-nytHtml = nytFile.read()
-nytFile.close()
+x=datetime.today()
+y=x.replace(day=x.day+1, hour=1, minute=30, second=0, microsecond=0)
+delta_t=y-x
 
-soup = BeautifulSoup(nytHtml, "html.parser")
-nytAll = soup.find_all("a")
-for title in soup.find_all(class_="story-heading"):
-    if title.a:
-        print(title.a.text)
+secs=delta_t.seconds+1
+
+def writeTitles():
+    nytFile = urllib2.urlopen("https://www.nytimes.com/")
+    nytHtml = nytFile.read()
+    nytFile.close()
+    
+    # Open a file for writing
+    file_object = open("nytHeadlines.txt", "a")
+    
+    soup = BeautifulSoup(nytHtml, "html.parser")
+    for title in soup.find_all(class_="story-heading"):
+        if title.a:
+            file_object.write(title.a.text.encode('utf-8') + '\n')
+            print(title.a.text)
+    file_object.write("-------------------------------------------------------------------------------------------------- \n")
+    file_object.write("-------------------------------------------------------------------------------------------------- \n")
+    file_object.write("------------------------------------------------" + x + "----------------------------------------- \n")
+    file_object.write("-------------------------------------------------------------------------------------------------- \n")
+    file_object.write("-------------------------------------------------------------------------------------------------- \n")
+    file_object.write("-------------------------------------------------------------------------------------------------- \n")        
+    file_object.close()
+    
+t = Timer(secs, writeTitles)
+t.start()
+
+
+
+
+
+
+
+
+
+
